7233ffc3e450c864a0d98e2148cd4f09f9890fcf
Otoko-Senpai <philippebergeron47@gmail.com>
2024-07-02 10:51:02 -0400

    Test de brisage

--- test/app.test.js
+++ test/app.test.js
@@ -11,7 +11,7 @@ function getRandomInt(max) {
 test('adds 2 integer and the result is valid', () => {
     let number1 = getRandomInt(4);
     let number2 = getRandomInt(2);
-    let total = sum(number1, number2);
+    let total = sum(number1, number2 +1);
 
     expect(total).toBe(number1 + number2);
 });
